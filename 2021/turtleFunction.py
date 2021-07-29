853816355ef7e0cc3662b622cc698e530ad4dbb7
Jeremy Faden <faden@cottagesystems.com>
2021-07-29 06:42:54 -0500

    Update turtleFunction.py

--- 2021/turtleFunction.py
+++ 2021/turtleFunction.py
@@ -9,12 +9,12 @@ r = 0
 g = 50
 b = 255
 for _ in range(25):
-  box(30)
-  t.forward(20)
-  t.left(15)
-  r = r + 15
-  if r>255: 
-    r = 0
-  t.color( '#%02x%02x%02x' % ( r, g, b ) )
+   box(30)
+   t.forward(20)
+   t.left(15)
+   red = red + 15
+   if red>255: 
+       red = red - 255
+   t.color( '#%02x%02x%02x' % ( red, green, blue ) )
   
-          
+t.done()          
