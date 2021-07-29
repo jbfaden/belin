d8e841f43ec58a2f156fd57901297bc2991a7f3c
Jeremy Faden <faden@cottagesystems.com>
2021-07-29 06:47:22 -0500

    Update forLoopTest.py

--- 2021/forLoopTest.py
+++ 2021/forLoopTest.py
@@ -1,7 +1,7 @@
 # This shows that you can use the same variable name "_" with nested loops
 i = 0
-for _ in range(10):
-    for _ in range(10):
+for _ in range(4):
+    for _ in range(5):
         i = i + 1
         print( i )
-    _ = 10    
+    _ = 5    
