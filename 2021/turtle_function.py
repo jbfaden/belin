c5579a834b74cc247cf4b7ba4853193dc6010e7d
Jeremy Faden <faden@cottagesystems.com>
2021-07-30 08:00:32 -0500

    Update turtle_function.py

--- 2021/turtle_function.py
+++ 2021/turtle_function.py
@@ -1,14 +1,14 @@
 import turtle as t
 
 def box(size):
-  for i in range(4):
+  for _ in range(4):
       t.forward(size)
       t.left(90)
 
-r = 0
-g = 50
-b = 255
-for i in range(25):
+red = 0
+green = 50
+blue = 255
+for _ in range(25):
    box(30)
    t.forward(20)
    t.left(15)
