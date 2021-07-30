341a4a6b914e420e8f4c0c0f3e6dcbd129b23f88
Jeremy Faden <faden@cottagesystems.com>
2021-07-29 21:07:17 -0500

    Update turtle_spiral.py

--- 2021/turtle_spiral.py
+++ 2021/turtle_spiral.py
@@ -1,21 +1,21 @@
-# How would you make a...
-#  ... triangle?
-#  ... pentagon?
-#  ... star?
-
 import turtle as t
+
 
 def draw_shape(size, num_sides, angle):
     """size is length of each side, num_sides is the number of sides, and angle is the amount to turn"""
     t.pendown()
-    for i in range(num_sides):
+    for _ in range(num_sides):
         t.forward(size)
         t.left(angle)
     t.penup()
 
+
+t.speed(100)
 size = 20
-for _ in range(120):
+for _ in range(20):
     draw_shape(size, 6, 60)
-    size = size * 0.99
+    t.forward(size * 2)
+    t.left(10)
+    size = size * 0.9
 
 t.done()
